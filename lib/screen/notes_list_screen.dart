2f49c97853b1f29e4b6d6b2279781606d6dc5687
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-06-21 02:00:46 +0430

    feat: create change layout button on notes list screen app bar

--- lib/screen/notes_list_screen.dart
+++ lib/screen/notes_list_screen.dart
@@ -16,21 +16,24 @@ class NotesListScreen extends StatefulWidget {
 
 class _NotesListScreenState extends State<NotesListScreen> {
   late MyDatabase db;
+  int axisCount = 2;
   @override
   Widget build(BuildContext context) {
     db = Provider.of<MyDatabase>(context);
 
     return Scaffold(
+      appBar: _getNoteListAppBar(),
       body: FutureBuilder<List<NoteData>>(
         future: _getNoteFromDatabase(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             List<NoteData> noteList = snapshot.data ?? [];
             if (noteList.isEmpty) {
-              return const Center(
+              return Center(
                 child: Text(
                   'No Notes Found!\nClick on add button to add new note.',
                   textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyLarge,
                 ),
               );
             }
@@ -41,8 +44,11 @@ class _NotesListScreenState extends State<NotesListScreen> {
               child: Text(snapshot.error.toString()),
             );
           }
-          return const Center(
-            child: Text('Click on add button to add new note.'),
+          return Center(
+            child: Text(
+              'Click on add button to add new note.',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
           );
         },
       ),
@@ -66,21 +72,29 @@ class _NotesListScreenState extends State<NotesListScreen> {
   }
 
   Widget noteListUI(List<NoteData> noteList) {
-    return GridView.builder(
-      gridDelegate:
-          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-      itemCount: noteList.length,
-      itemBuilder: (context, index) => NoteGridTile(
-        noteData: noteList[index],
-        onTap: () => _navigateToDetail(
-          context,
-          'Edit Note',
-          NoteCompanion(
-            id: Value(noteList[index].id),
-            title: Value(noteList[index].title),
-            description: Value(noteList[index].description),
-            color: Value(noteList[index].priority),
-            priority: Value(noteList[index].color),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GridView.builder(
+        physics: const BouncingScrollPhysics(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: axisCount == 2 ? 2 : 1,
+          childAspectRatio: axisCount == 2 ? 1 : 3,
+          crossAxisSpacing: 5,
+          mainAxisSpacing: 5,
+        ),
+        itemCount: noteList.length,
+        itemBuilder: (context, index) => NoteGridTile(
+          noteData: noteList[index],
+          onTap: () => _navigateToDetail(
+            context,
+            'Edit Note',
+            NoteCompanion(
+              id: Value(noteList[index].id),
+              title: Value(noteList[index].title),
+              description: Value(noteList[index].description),
+              color: Value(noteList[index].priority),
+              priority: Value(noteList[index].color),
+            ),
           ),
         ),
       ),
@@ -103,5 +117,33 @@ class _NotesListScreenState extends State<NotesListScreen> {
     if (res != null && res == true) {
       setState(() {});
     }
+  }
+
+  _getNoteListAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      centerTitle: true,
+      elevation: 0,
+      title: Text(
+        'Notes',
+        style: Theme.of(context).textTheme.titleLarge,
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {
+            if (axisCount == 2) {
+              axisCount = 1;
+            } else {
+              axisCount = 2;
+            }
+            setState(() {});
+          },
+          icon: Icon(
+            axisCount == 1 ? Icons.grid_view_rounded : Icons.list_alt_rounded,
+            color: Colors.black,
+          ),
+        ),
+      ],
+    );
   }
 }
