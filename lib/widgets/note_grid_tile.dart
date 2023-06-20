bc215b5d347ff8575963043463dead135e2848bc
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-06-20 19:52:07 +0430

    feat: create note grid tile widget

---
+++ lib/widgets/note_grid_tile.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+import '../database/database.dart';
+
+class NoteGridTile extends StatelessWidget {
+  final NoteData noteData;
+  final Function() onTap;
+  const NoteGridTile({
+    required this.noteData,
+    required this.onTap,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.symmetric(
+          horizontal: 10,
+          vertical: 8,
+        ),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 10,
+          vertical: 8,
+        ),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            border: Border.all(color: Colors.black)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  noteData.title,
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+                Text(
+                  _getPriority(noteData.priority),
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Expanded(
+              child: Text(
+                noteData.description,
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  '12/12/2021',
+                  style: Theme.of(context).textTheme.labelSmall,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _getPriority(int priority) {
+    switch (priority) {
+      case 1:
+        return '!!!';
+      case 2:
+        return '!!';
+      default:
+        return '!';
+    }
+  }
+}
